1d5e6165a040d4679107696fbd676a89d1b08ed7
bskr2020 <bskr.2020@gmail.com>
2021-05-09 07:46:28 +0530

    java first commit.

---
+++ src/main/java/Testdevops.java
@@ -0,0 +1,8 @@
+import javax.print.attribute.standard.PrinterURI;
+
+public class Testdevops {
+    public static void main(String[] args) {
+    System.out.println("Hello ..");
+    System.out.println("Welcome to github and jenkins integration ..");
+    }
+}
